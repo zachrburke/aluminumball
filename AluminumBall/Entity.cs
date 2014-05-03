c0ae56f6eddc4641bed882f6bf44b0c83f85610b
Zach Burke <madutilitybelt@gmail.com>
2014-05-03 17:57:15 -0500

    initial commit

---
+++ AluminumBall/Entity.cs
@@ -0,0 +1,18 @@
+﻿using AluminumBall.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AluminumBall
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Components = new List<IComponent>();
+        }
+
+        public List<IComponent> Components { get; set; }
+    }
+}
---
+++ AluminumBall/GameState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AluminumBall
+{
+    public class GameState
+    {
+        public Dictionary<Guid, Entity> Entities { get; set; }
+    }
+}
